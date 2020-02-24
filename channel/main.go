be4dac30ba747e106023a073bcfe2c20e5c898a5
shc <shc@shcdeMacBook-Pro.local>
2020-02-24 18:20:03 +0800

    feat: 添加context模块

--- channel/main.go
+++ channel/main.go
@@ -7,8 +7,13 @@ var ch2 chan int
 var chs = []chan int{ch1, ch2}
 var numbers = []int{1, 2, 3, 4, 5}
 
-func main () {
+func main() {
 
+	// 1、select会执行一个分支
+	// 2、如果有default，且其他均不满足时，会执行default，然后结束，如果没有default，则会阻塞，直到可以执行
+	// 3、如果同时满足多个分支，则随机选取一个执行
+	// 4、所有channel表达式都会被求值、所有被发送的表达式都会被求值。求值顺序：自上而下、从左到右
+	// 待验证：当既存在均不满足，且无default的多channel表达式时（select阻塞），channel表达式会被执行几次
 	select {
 	case getChan(1) <- getNumber(1):
 		fmt.Println("1th case is selected.")
@@ -26,6 +31,6 @@ func getNumber(i int) int {
 
 func getChan(i int) chan int {
 	fmt.Printf("chs[%d]\n", i)
-	fmt.Println("--",chs[i],"--")
+	fmt.Println("--", chs[i], "--")
 	return chs[i]
 }
---
+++ context/code/context.go
@@ -0,0 +1,10 @@
+package code
+
+import (
+	"context"
+	"math/rand"
+)
+
+func CountTest(ctx context.Context) {
+	rand.Intn()
+}
